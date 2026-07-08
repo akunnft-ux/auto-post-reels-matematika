26608652b6ec4e508bf9e9e513a0a8a2c2b13e3a
akunnft-ux <akunnft@gmail.com>
2026-07-08 10:10:38 -0400

    fix: change TTS voice back to ArdiNeural (male) and rate to +22%

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ IMG_WIDTH = 1080
 IMG_HEIGHT = 1920
 FPS = 24
 
-TTS_VOICE = "id-ID-GadisNeural"
-TTS_RATE = "+45%"
+TTS_VOICE = "id-ID-ArdiNeural"
+TTS_RATE = "+22%"
 TTS_TIMEOUT = 30
 TTS_MAX_CHARS = 2000
 _EMOJI_RE = re.compile(
